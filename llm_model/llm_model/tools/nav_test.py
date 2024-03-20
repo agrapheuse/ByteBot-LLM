53c37d3d277652e98b60b855ab8cf01a64dfb635
fibleep <filipn924@gmail.com>
2024-03-20 10:14:21 +0100

    update imports

--- llm_model/llm_model/tools/nav_test.py
+++ llm_model/llm_model/tools/nav_test.py
@@ -5,7 +5,7 @@ import rclpy
 from nav2_simple_commander.robot_navigator import BasicNavigator
 from rclpy.node import Node
 from std_msgs.msg import String
-from turtlebot4_navigation.turtlebot4_navigator import (
+from turtlebot4_navigation import (
     TurtleBot4Directions,
     TurtleBot4Navigator,
 )
